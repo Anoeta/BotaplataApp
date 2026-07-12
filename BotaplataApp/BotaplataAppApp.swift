f393ec67d23996f573fe6c9a6c14de4141c967bf
Daniel Moreira <danielmoreira@live.fr>
2026-07-12 16:55:35 +0200

    Initial commit: Xcode SwiftUI project

---
+++ BotaplataApp/BotaplataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotaplataAppApp.swift
+//  BotaplataApp
+//
+//  Created by tahoe on 12/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct BotaplataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
